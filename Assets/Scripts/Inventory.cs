ad3e11666ffac1b81aa26565af5284f652cacf36
Eric <e-swartz@live.com>
2016-06-30 11:09:07 -0400

    Testing UI

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+    
+    private RectTransform inventoryRect;
+    private float inventoryWidth, inventoryHeight;
+    private List<GameObject> allSlots;
+
+    public int slots;
+    public int rows;
+    public float slotPadingLeft, slotPaddingTop;
+    public float slotSize;
+    public GameObject slotPrefab;
+
+	// Use this for initialization
+	void Start () {
+        CreateLayout();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void CreateLayout()
+    {
+        allSlots = new List<GameObject>();
+
+        inventoryWidth = (slots / rows) + (slotSize + slotPadingLeft) + slotPadingLeft;
+        inventoryHeight = rows * (slotSize + slotPaddingTop) + slotPaddingTop;
+
+        inventoryRect = GetComponent<RectTransform>();
+
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryWidth);
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, inventoryHeight);
+
+
+    }
+}
